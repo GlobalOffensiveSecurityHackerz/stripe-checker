3de8206b533aa464943fdaa7c92abece24f1ebd8
SnisterRPA <33238696+GlobalOffensiveSecurityHackerz@users.noreply.github.com>
2022-08-29 15:54:50 +0800

    Update switchblade.php

--- switchblade.php
+++ switchblade.php
@@ -6,7 +6,7 @@
 // Webhook: https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
 // BOT API Configuration
-$botToken = "TOKEN"; #<------------------- PUT YOUR TOKEN HERE------------->#
+$botToken = "5730054104:AAFjqnBteM_eKJqo7beo5rqGPgg3oTdRsG0";
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
